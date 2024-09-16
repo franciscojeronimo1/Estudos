02a59128d3898d920c3b4aa620f2f656b0a6adb4
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2024-09-16 15:18:34 -0300

    .

--- node/aula14/server.js
+++ node/aula14/server.js
@@ -1,5 +1,16 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.CONNECTIONSTRING)
+    .then( () => {
+        console.log('Conectei a base de dados')
+        app.emit('pronto')
+    }) 
+    .catch(e => console.log(e))
+
 const routes = require('./routes')
 const path = require('path')
 const { middlewareGlobal} = require('./src/middlewares/middleware')
@@ -14,9 +25,10 @@ app.set('view engine', 'ejs')
 //Nossos próprios middlewares
 app.use(middlewareGlobal)
 app.use(routes)
-
-
+app.on('pronto' , () =>{   
 app.listen(3000, () => {
     console.log('Acessar http://localhost:3000')
     console.log('Servidor executando na porta 3000')
 })
+})
+
