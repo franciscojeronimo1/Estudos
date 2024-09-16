d827a3fc6e56a6f004f6c922c894448686fe2ce8
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2024-09-16 20:17:17 -0300

    .

--- node/aula15/server.js
+++ node/aula15/server.js
@@ -10,6 +10,12 @@ mongoose.connect(process.env.CONNECTIONSTRING)
     }) 
     .catch(e => console.log(e))
 
+
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const flash = require('connect-flash')
+
+
 const routes = require('./routes')
 const path = require('path')
 const { middlewareGlobal} = require('./src/middlewares/middleware')
@@ -17,6 +23,19 @@ const { middlewareGlobal} = require('./src/middlewares/middleware')
 app.use(express.urlencoded({ extended: true }))
 
 app.use(express.static(path.resolve(__dirname, 'public')))
+
+const sessionOptions = session({
+    secret: '4jkjasd43432jsadsa432',
+    store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
+    resave: false,
+    saveUninitialized:false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+})
+app.use(sessionOptions)
+app.use(flash())
 
 app.set('views', path.resolve(__dirname, 'src', 'views'))
 app.set('view engine', 'ejs')
