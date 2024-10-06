53527865c2560a487498e54487f0dd7126ef542d
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2024-10-06 15:12:08 -0300

    new

---
+++ react/react-base/src/pages/Page404/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from '../../styles/globalStyles';
+
+export default function Page404() {
+  return (
+    <Container>
+      <h1>Essa página nao existe</h1>
+    </Container>
+  );
+}
