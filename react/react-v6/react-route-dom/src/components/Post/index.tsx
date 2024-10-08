4d4185cffc742b41bd36b3ce3453ed6990746c67
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2024-10-07 21:49:23 -0300

    Rota Alinhada

--- react/react-v6/react-route-dom/src/components/Post/index.tsx
+++ react/react-v6/react-route-dom/src/components/Post/index.tsx
@@ -1,13 +1,8 @@
-import { useParams, useSearchParams } from 'react-router-dom'
-
 export const Post = () => {
-  const params = useParams()
-  const { id } = params
-  const [qs] = useSearchParams()
-
   return (
     <div>
-      <h1>Post { `param: ${id}`} {`QS: ${qs.get('page')}`} </h1>
+      <h1>Post</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt laborum, perferendis non nostrum beatae corrupti eaque qui omnis ipsum totam ex eligendi voluptate asperiores adipisci itaque debitis aliquam quisquam cupiditate.</p>
     </div>
   )
 }
