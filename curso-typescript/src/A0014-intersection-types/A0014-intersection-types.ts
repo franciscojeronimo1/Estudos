d0c02cfb5f09e1a3fbfdc01c4413a48a262f62d7
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2024-10-28 22:34:02 -0300

    ;

--- curso-typescript/src/A0014-intersection-types/A0014-intersection-types.ts
+++ curso-typescript/src/A0014-intersection-types/A0014-intersection-types.ts
@@ -3,16 +3,17 @@ type TemSobrenome = { sobrenome: string };
 type TemIdade = { idade: number };
 type Pessoa = TemNome & TemSobrenome & TemIdade; // AND
 
-type AB = 'A' | 'B';
+/*type AB = 'A' | 'B';
 type AC = 'A' | 'C';
 type AD = 'D' | 'A';
 type Intersecao = AB & AC & AD;
-
+*/
 const pessoa: Pessoa = {
   nome: 'Luiz',
   sobrenome: 'Miranda',
   idade: 30,
 };
+
 
 console.log(pessoa);
 
