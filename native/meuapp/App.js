c5b7dffd4236d01f4d284293f6fd87447156218e
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-03-16 15:29:30 -0300

    ,

--- native/meuapp/App.js
+++ native/meuapp/App.js
@@ -1,33 +1,52 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      nome: ''
-    }
+      nome: '',
 
-    this.entrar = this.entrar.bind(this);
+    };
+
+    this.pegaNome = this.pegaNome.bind(this)
   }
-
-  entrar(nome) {
-    this.setState({
-      nome: nome
-    })
+  pegaNome(texto) {
+    this.setState({ nome: texto })
   }
 
   render() {
     return (
-      <View style={{ marginTop: 30 }}>
-        <Button title="Entrar" onPress={() => this.entrar('Matheus')} />
-        <Text style={{ fontSize: 23, color: 'red', textAlign: 'center' }}>{this.state.nome}</Text>
+      <View style={styles.container}>
 
-      </View>
+        <TextInput
+          style={styles.input}
+          placeholder={'Digite seu nome?'}
+          onChangeText={this.pegaNome}
+        />
+        <Text style={styles.texto}>Bem Vindo {this.state.nome}</Text>
+      </View >
     )
   }
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  input: {
+    height: 45,
+    borderWidth: 1,
+    borderColor: '#222',
+    margin: 10,
+    marginTop: 50,
+    fontSize: 20,
+    padding: 10,
+  },
+  texto: {
+    textAlign: 'center',
+    fontSize: 25,
+  }
+})
 
 export default App
