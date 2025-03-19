182ebe63b92651e2f91abb6d4de49050b577da94
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-03-19 16:23:34 -0300

    ,

--- native/aprendendoMais/mais/App.js
+++ native/aprendendoMais/mais/App.js
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-    </View>
-  );
+class App extends Component() {
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <scrollView>
+          <View style={styles.box1}></View>
+          <View style={styles.box2}></View>
+          <View style={styles.box3}></View>
+          <View style={styles.box4}></View>
+        </scrollView>
+
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  box1: {
+    backgroundColor: 'red',
+    height: 350,
+  },
+  box2: {
+    backgroundColor: 'green',
+    height: 350,
+  },
+  box3: {
+    backgroundColor: 'yellow',
+    height: 350,
+  },
+  box4: {
+    backgroundColor: 'blue',
+    height: 350,
+  },
 });
+
+export default App;
