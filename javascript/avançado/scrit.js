2a1dc7d29c9227414f9b52bb302137da298549f4
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-04-03 11:25:58 -0300

    desconstruindo

---
+++ javascript/avançado/scrit.js
@@ -0,0 +1,27 @@
+
+
+
+let pessoa = {
+  nome: "Matheuys",
+  sobrenome: "Fraga",
+  empresa: "Sujeito programador",
+  cargo: "Programador"
+}
+
+// const { nome: nomePessoa, cargo, sobrenome, empresa } = pessoa
+
+// console.log(nomePessoa)
+// console.log(cargo)
+// console.log(empresa)
+
+let nome = ["Matheus", "lucas", "Henrique"]
+
+// let { 0: matheusss, 1: lucao } = nome
+
+// console.log(matheusss)
+// console.log(lucao)
+
+
+let [prim, segund] = nome
+
+console.log(prim)
