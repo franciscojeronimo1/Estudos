8e2e870ca409a3cbcbc081b1954f26038fcb831e
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-04-06 18:00:34 -0300

    Update App.js

--- react/react-test/my-app/src/App.js
+++ react/react-test/my-app/src/App.js
@@ -1,10 +1,57 @@
-import Nome from './components/Nome';
+import { useState } from 'react'
+
+
 
 function App() {
+  const [nome, setNome] = useState('')
+  const [email, setEmail] = useState('')
+  const [idade, setIdade] = useState('')
+  const [user, setUser] = useState({})
+
+
+  function handleRegister(e) {
+    e.preventDefault()
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email,
+    })
+  }
+
   return (
     <div>
-      <h1>bem vindo</h1>
-      <Nome aluno="lucas" idade={31} />
+      <h1>Cadastrando usuario</h1>
+
+      <form onSubmit={handleRegister}>
+        <label >Nome: </label><br />
+        <input
+          placeholder="Digite seu nome"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        /><br />
+
+        <label >Email: </label><br />
+        <input
+          placeholder="Digite seu Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} /><br />
+
+        <label >idade: </label><br />
+        <input
+          placeholder="Digite sua idade"
+          value={idade}
+          onChange={(e) => setIdade(e.target.value)}
+        /><br />
+
+        <button type="submit">registrar</button>
+      </form>
+      <br /><br />
+
+      <div>
+        <span>Bem vindo: {user.nome}</span><br />
+        <span>idade: {user.idade}</span><br />
+        <span>Email: {user.email} </span><br />
+      </div>
     </div>
   );
 }
