9b8443c015a08529f4a99ab7b06d6b0f6fa6e501
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-04-08 11:20:06 -0300

    filme

--- react/newfilmes/src/pages/Home/index.js
+++ react/newfilmes/src/pages/Home/index.js
@@ -1,4 +1,26 @@
+import { useEffect, useState } from 'react'
+import api from '../../services/api'
+
 function Home() {
+  const [filmes, setFilmes] = useState([])
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: "60a4701284dd8ee23204746d9803ae03",
+          language: "pt-BR",
+          page: 1,
+        }
+      })
+
+      console.log(response.data.results)
+
+    }
+
+    loadFilmes()
+  }, [])
+
   return (
     <div>
       <h1>BEM VINDO AO HOME</h1>
