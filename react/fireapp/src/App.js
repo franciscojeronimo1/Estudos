a19a3d98d23811f3b4aa427b8a4aa7dfff232c81
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-04-13 23:20:06 -0300

    Logando usuários

--- react/fireapp/src/App.js
+++ react/fireapp/src/App.js
@@ -1,6 +1,7 @@
-import { useState } from 'react'
-import { db } from './firebaseConnection';
-import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc } from 'firebase/firestore'
+import { useState, useEffect } from 'react'
+import { db, auth } from './firebaseConnection';
+import { doc, setDoc, collection, addDoc, getDoc, getDocs, updateDoc, deleteDoc, onSnapshot } from 'firebase/firestore'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth'
 import './app.css'
 
 function App() {
@@ -8,7 +9,51 @@ function App() {
   const [autor, setAutor] = useState('')
   const [idPost, setIdPost] = useState('')
 
+  const [email, setEmail] = useState('')
+  const [senha, setSenha] = useState('')
+
+  const [user, setUser] = useState(false)
+  const [userDetail, setUserDetail] = useState([])
+
   const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    async function loadPosts() {
+      const unsub = onSnapshot(collection(db, "posts"), (snapshopt) => {
+        let listaPost = []
+
+        snapshopt.forEach((doc) => {
+          listaPost.push({
+            id: doc.id,
+            titulo: doc.data().titulo,
+            autor: doc.data().autor
+          })
+        })
+
+        setPosts(listaPost)
+      })
+    }
+
+    loadPosts()
+  }, [])
+
+  useEffect(() => {
+    async function checkLogin() {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setUser(true)
+          setUserDetail({
+            uid: user.uid,
+            email: user.email
+          })
+        } else {
+          setUser(false)
+          setUserDetail({})
+        }
+      })
+    }
+    checkLogin()
+  }, [])
 
   async function handleAdd() {
     // await setDoc(doc(db, "posts", "12345"), {
@@ -87,12 +132,88 @@ function App() {
       })
   }
 
+  async function excluirPost(id) {
+    const docRef = doc(db, "posts", id)
+    await deleteDoc(docRef)
+  }
+
+  async function novoUsuario() {
+    await createUserWithEmailAndPassword(auth, email, senha)
+      .then((value) => {
+        console.log('Sucesso no cadastro')
+        setEmail('')
+        setSenha('')
+      })
+      .catch((error) => {
+        if (error.code === 'auth/weak-password') {
+          alert("Senha muito fraca")
+        } else if (error.code === 'auth/email') {
+          alert('Email ja existe')
+        }
+      })
+  }
+
+  async function logarUsuario() {
+    await signInWithEmailAndPassword(auth, email, senha)
+      .then((value) => {
+        console.log('USER LOGADO COM SUCESSO')
+        console.log(value.user)
+
+        setUserDetail({
+          uid: value.user.uid,
+          email: value.user.email,
+
+        })
+        setUser(true)
+
+        setEmail('')
+        setSenha('')
+      })
+  }
+
+  async function fazerLogout() {
+    await signOut(auth)
+    setUser(false)
+    setUserDetail({})
+  }
+
   return (
     <div >
       <h1>ReactJS + Firebase </h1>
 
-      <div className="container">
+      {user && (
+        <div>
+          <strong>Seja bem vindo(a) (Você está logado!)</strong> <br />
+          <span>ID: {userDetail.uid} - Email: {userDetail.email}</span> <br />
+          <button onClick={fazerLogout}>sair da conta</button>
+          <br /> <br />
+        </div>
+      )}
 
+      <div className="container">
+        <h2>Usuarios</h2>
+        <label >Email</label>
+        <input value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Digite um email"
+        />
+        <br />
+
+        <label >Senha</label>
+        <input value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+          placeholder="informe sua senha" />
+        <br />
+
+        <button onClick={novoUsuario}>Cadastras</button>
+        <button onClick={logarUsuario}>Fazer login</button>
+      </div><br />
+
+      <br />
+      <br />
+      <hr />
+      <div className="container">
+        <h2>Posts</h2>
         <label>ID do Post:</label>
         <input
           placeholder='Digite o ID do post'
@@ -117,6 +238,7 @@ function App() {
                 <strong>ID: {post.id}</strong><br />
                 <span>Titulo:{post.titulo} </span> <br />
                 <span>Autor: {post.autor}</span><br /><br />
+                <button onClick={() => excluirPost(post.id)}>Excluir</button> <br /> <br />
 
               </li>
             )
