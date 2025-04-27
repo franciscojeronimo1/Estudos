11fd6662ea4b2040f2b53c2056873af1282723ec
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-04-27 08:28:11 -0300

    cript

---
+++ react-ts/criptos/src/pages/detail/index.tsx
@@ -0,0 +1,7 @@
+export function Detail () {
+  return(
+    <div>
+      <h1>Pagina Detalhe da moeda</h1>
+    </div>
+  )
+}
---
+++ react-ts/criptos/src/pages/notfound/index.tsx
@@ -0,0 +1,7 @@
+export function Notfound () {
+  return(
+    <div>
+      <h1>Página 404, está pagina nao existe!</h1>
+    </div>
+  )
+}
