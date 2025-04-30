9aafd3b8f9b630ff1a517c8f2234e983b4d581c2
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-04-30 13:03:45 -0300

    admin

---
+++ typescript/linktree/src/routes/Private.tsx
@@ -0,0 +1,44 @@
+import { ReactNode, useState, useEffect } from 'react'
+import {auth} from '../services/firebaseConnection'
+import { onAuthStateChanged } from 'firebase/auth'
+import { Navigate } from 'react-router-dom'
+
+interface PrivateProps{
+  children: ReactNode
+}
+
+export function Private({children}: PrivateProps ): any {
+  const [loading, setLoading] = useState(true)
+  const [signed,setSigned] = useState(false)
+
+  useEffect(()=> {
+
+    const unsub = onAuthStateChanged(auth, (user)=> {
+      if(user) {
+        const useData ={
+          uid: user?.uid,
+          email: user?.email
+        }
+
+        localStorage.setItem("@reactlinks", JSON.stringify(useData))
+        setLoading(false)
+        setSigned(true)
+      }else {
+        setLoading(false)
+        setSigned(false)
+      }
+    })
+
+    return () => {
+      unsub()
+    }
+  },[])
+  if(loading) {
+    return <div></div>
+  }
+
+  if(!signed) {
+    return <Navigate to="/login"/>
+  }
+  return children
+}
