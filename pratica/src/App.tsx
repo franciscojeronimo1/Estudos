25c70450e13619ae5e36fe15950c6215bf31a213
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-05-04 10:09:18 -0300

    Update App.tsx

--- pratica/src/App.tsx
+++ pratica/src/App.tsx
@@ -3,12 +3,17 @@ import { useState } from 'react'
 
 
 function App() {
-  
+  const [nome, setNome] = useState('')
+
+  function handleName() {
+    const name = nome
+  }
+
   return (
     <div>
-      <input type="text" placeholder='digite seu nome'/>
-      <button>clique</button>
-      <h1>Ola Francisco</h1>
+      <input type="text" placeholder='digite seu nome' value={nome}  onChange={(e)=> setNome(e.target.value)}/>
+      <button onClick={handleName} >clique</button>
+      <h1>Ola {nome}</h1>
     </div>
   )
 }
