bde072cd4b284527c14cc39811f84a1a44afe0b8
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-05-14 12:51:47 -0300

    d

--- react/chronos/src/main.tsx
+++ react/chronos/src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 
+import { Login } from './pages/Login'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App/>
+    <Login/>
+    
   </StrictMode>,
 )
