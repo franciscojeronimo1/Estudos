fb74a33d528b8f4c2cc5da9e2717486c39f00bfb
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-05-16 16:24:26 -0300

    ,

--- site-anime/src/components/Header/index.tsx
+++ site-anime/src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 export function Header() {
   return(
-    <header>
-      <h1>Header</h1>
+    <header className='bg-black w-auto max-h-30 h-50'>
+      <h1 className='font-bold text-center text-5xl text-white' >Animes</h1>
     </header>
   )
 }
--- site-anime/src/pages/Home/index.tsx
+++ site-anime/src/pages/Home/index.tsx
@@ -1,10 +1,47 @@
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
+interface CharacterProps {
+  id: string;
+  name: string;
+  ki: string;
+  maxKi: string;
+  race: string;
+}
+
+interface ItemProps{
+  items: CharacterProps[]
+}
 
 export function Home() {
+  const [character, setCharacter] = useState<CharacterProps[]>([])
+
+  useEffect(() => {
+    getData()
+  },[])
+
+  async function getData() {
+    fetch("https://dragonball-api.com/api/characters?limit=10")
+    .then(response => response.json())
+    .then((data: ItemProps ) => {
+      const characterItem = data.items
+
+      setCharacter(characterItem)
+    })
+  }
+
   return(
-    <div><h1>Home
+    <div key={}><h1>Home
       <p><Link to="/detail">Detalhes</Link></p>
-      </h1></div>
+      </h1>
+      
+      <div>
+      {character.length > 0 && character.map((item) => (
+        <div key={item.id}><p>nomes {item.name}</p></div>
+      ))}
+
+      </div>
+      </div>
   )
 }
 
