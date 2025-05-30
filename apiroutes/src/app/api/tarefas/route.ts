92a4452393443576b0a0934092d9e0e576443989
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-05-30 08:09:19 -0300

    Update route.ts

--- apiroutes/src/app/api/tarefas/route.ts
+++ apiroutes/src/app/api/tarefas/route.ts
@@ -5,3 +5,31 @@ let tarefas = [' Comprar coca', 'estudar nextJS']
 export async function GET(request: Request) {
     return NextResponse.json(tarefas)
 }
+
+export async function POST(request: Request){
+    const data = await request.json()
+    tarefas.push(data.name)
+
+    return NextResponse.json(tarefas)
+}
+
+export async function PUT(request: Request){
+    const { searchParams} = new URL(request.url)
+    const index = searchParams.get('index')
+
+    const data = await request.json()
+
+    tarefas[Number(index)] = data.name
+
+    return NextResponse.json({message: "tarefa atualizado com sucesso!"})
+}
+
+export async function DELETE(request:Request) {
+    const {searchParams} = new URL(request.url)
+    const index = searchParams.get("index")
+
+    tarefas.splice(Number(index), 1)
+
+    return NextResponse.json({message: "Tarefa deletada com sucesso"})
+    
+}
