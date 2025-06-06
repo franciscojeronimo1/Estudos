5c959885d908d408daa681404e85e482f8cd1e93
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-06-06 11:53:06 -0300

    finalizado

--- dailygames/src/app/profile/page.tsx
+++ dailygames/src/app/profile/page.tsx
@@ -2,6 +2,14 @@ import { Container } from "../components/container";
 import Image from 'next/image'
 import UserImg from '../../../public/user.png'
 import { FaShareAlt } from "react-icons/fa";
+import { FavoriteCard } from "./components/favorite";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Meu Perfil - Daly Games sua plataforma de jogos!",
+    description: "Perfil de Dev"
+}
+
 export default function Profile() {
     return(
         <main className="w-full text-black">
@@ -19,6 +27,20 @@ export default function Profile() {
                     <div className="sm:absolute top-0 right-0 gap-3 flex items-center justify-center mt-2">
                         <button className="bg-gray-700 px-4 py-3 rounded-lg text-white">configuraçoes</button>
                         <button className="bg-gray-700 px-4 py-3 rounded-lg"><FaShareAlt size={24} color="#FFF"/></button>
+                    </div>
+                </section>
+
+                <section className="flex flex-wrap gap-5 flex-col md:flex-row">
+                    <div className="flex-grow flex-wrap">
+                       <FavoriteCard/> 
+                    </div>
+
+                      <div className="flex-grow flex-wrap">
+                       <FavoriteCard/> 
+                    </div >
+
+                      <div className="flex-grow flex-wrap">
+                       <FavoriteCard/> 
                     </div>
                 </section>
             </Container>
