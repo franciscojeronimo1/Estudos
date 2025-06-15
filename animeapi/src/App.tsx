b03008097d519d7c627286dc148fa25ad2257421
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-06-15 20:29:04 -0300

    z

---
+++ animeapi/src/App.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Link to='/contato'>contato</Link>
+       <h1>teste</h1>
+    </>
+  )
+}
+
+export default App
