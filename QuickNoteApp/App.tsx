6f81e5291a2df12b1103d67d1eef029c1c861cb6
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-06-17 00:49:15 -0300

    Quick Note

---
+++ QuickNoteApp/App.tsx
@@ -0,0 +1,13 @@
+// App.tsx
+import React from 'react';
+import QuickNoteApp from './QuickNote'; // Certifique-se de que o caminho está correto
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <QuickNoteApp />
+    </>
+  );
+}
