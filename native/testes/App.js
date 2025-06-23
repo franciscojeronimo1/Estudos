75673d4dd796f291969aaa37ea87535d7b92e101
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-06-22 23:46:11 -0300

    .

---
+++ native/testes/App.js
@@ -0,0 +1,21 @@
+import {View, Text, Image} from 'react-native'
+import React, {Component} from 'react'
+
+
+class App extends Component {
+  render(){
+
+    let nome = 'Catay'
+   
+    return(
+      <View style={{backgroundColor: 'white', height: '100%'}}>
+        <Text> Catau</Text>
+
+      </View>
+    )
+  }
+
+
+}  
+
+export default App
