a5550beb84bca1d840d2aa966ba12bc082bb4697
Francisco Jeronimo <franciscojeronimo1@hotmail.com>
2025-06-22 23:58:05 -0300

    ,

--- native/testes/App.js
+++ native/testes/App.js
@@ -1,15 +1,31 @@
-import {View, Text, Image} from 'react-native'
+import {View, Text, Button} from 'react-native'
 import React, {Component} from 'react'
 
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      nome: ''
+    }
+    this.entrar = this.entrar.bind(this)
+  }
+  
+  entrar(nome) {
+    this.setState({
+      nome: nome
+    })
+  }
+  
   render(){
 
-    let nome = 'Catay'
    
     return(
-      <View style={{backgroundColor: 'white', height: '100%'}}>
-        <Text> Catau</Text>
+      <View style={{backgroundColor: 'white', height: '100%', marginTop:30}}>
+        <Button title='Entrar' onPress={ () => this.entrar('Francisco')}/>
+        <Text style={{fontSize:23, color:'red', textAlign: 'center'}}> 
+          {this.state.nome}
+          </Text>
 
       </View>
     )
